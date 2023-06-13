64835d7ade9b973101110ed9563c47687d0bbc2e
Nidala96 github <pittaufabio2@gmail.com>
2023-06-13 15:53:07 +0200

    Aggiunti ram e hard-disk nei campi del html

--- src/main/resources/static/js/pc.js
+++ src/main/resources/static/js/pc.js
@@ -18,7 +18,9 @@ function modifyPc(element)
 	document.getElementById("modSchedaMadre").value = element.getAttribute('data-schedaMadre');
 	document.getElementById("modCasePc").value = element.getAttribute('data-casePc');
 	document.getElementById("modScontato").value = element.getAttribute('data-percentualeSconto') == 1 ? 0 : element.getAttribute('data-percentualeSconto') * 100;
-	
+	document.getElementById("modRam").value = element.getAttribute('data-ram');
+    document.getElementById("modHardDisk").value = element.getAttribute('data-hardDisk');
+
  	
 }
 
