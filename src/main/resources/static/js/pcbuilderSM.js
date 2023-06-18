2ed761a54d691c773c1d76e7d2428ed63377069b
baldi364 <baldi364@gmail.com>
2023-06-18 19:48:56 +0200

    aggiunto un html mancante modificato

--- src/main/resources/static/js/pcbuilderSM.js
+++ src/main/resources/static/js/pcbuilderSM.js
@@ -1,29 +1,14 @@
+// TABELLA SCHEDA MADRE
+document.getElementById("modalNewItemSM").addEventListener("click", function() {
+    document.getElementById("modal-addSM").style.display = "block";
+});
 
-document.getElementById("modalNewItemSM").onclick = function()
-{
-    console.log(document.getElementById("modalNewItemSM"));
-	document.getElementById("modal-addSM").style.display = "block";
-}
+document.getElementById("closeAddSM").addEventListener("click", function() {
+    document.getElementById("modal-addSM").style.display = "none";
+});
 
-document.getElementById("closeAddSM").onclick = function()
-{
-	document.getElementById("modal-addSM").style.display = "none";
-}
-
-document.getElementById("closeModSM").onclick = function()
-{
-	document.getElementById("modal-modSM").style.display = "none";
-}
-
-window.onclick = function(event)
-{
-	if(event.target == document.getElementById("modal-modSM"))
-	{
-		document.getElementById("modal-modSM").style.display = "none";
-	}
-
-	if(event.target == document.getElementById("modal-addSM"))
-	{
-		document.getElementById("modal-addSM").style.display = "none";
-	}
-}
+window.addEventListener("click", function(event) {
+    if (event.target == document.getElementById("modal-addSM")) {
+        document.getElementById("modal-addSM").style.display = "none";
+    }
+});
